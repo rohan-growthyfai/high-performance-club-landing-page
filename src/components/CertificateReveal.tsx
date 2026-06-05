1605a5739696b5500e28185d31fef91315b8ee4f
RohanM <irohanmote@gmail.com>
2026-06-05 11:26:44 +0530

    fix: sticky note in certificate section auto-sizes to text content

--- src/components/CertificateReveal.tsx
+++ src/components/CertificateReveal.tsx
@@ -49,8 +49,8 @@ export default function CertificateReveal() {
           <div className="lg:col-span-7 relative flex justify-center">
 
             {/* Sticky note */}
-            <div className="absolute -top-6 -left-2 lg:-left-6 z-20 sticky-note p-2.5 rounded-md tilt-left w-36 hidden sm:block">
-              <p className="font-serif italic text-xs text-amber-900 leading-snug">
+            <div className="absolute -top-6 -left-2 lg:-left-6 z-20 sticky-note p-2.5 rounded-md tilt-left inline-block hidden sm:inline-block">
+              <p className="font-serif italic text-xs text-amber-900 leading-snug whitespace-nowrap">
                 Finish at least 5 days &amp; this is yours!
               </p>
             </div>
